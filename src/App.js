f769eeba55c6715e6be0d311741aee4fbefbfe7b
aleksandrsg <aleksandrs.galiullins@gmail.com>
2021-09-11 14:08:04 +0100

    Added Input Field for Serch

--- src/App.js
+++ src/App.js
@@ -24,7 +24,16 @@ function App() {
             </ul>
         </header>
         <div className="content p-40">
-          <h1 className="mb-40">All Sneakers</h1>
+          <div className="d-flex justify-between align-center mb-40"> 
+            <h1>All Sneakers</h1>
+            <div className="search-block d-flex align-center">
+              <img src="img/search.svg" alt="Search" />
+              <input type="text" placeholder="Search sneakers"/>
+            </div>
+          </div>
+          
+
+
           <div className="d-flex">
           <div className="card">
             <img width={133} heigth={112} src="img/sneakers/1.jpg" alt="Sneakers" />
