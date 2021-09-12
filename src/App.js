e74e45e241f6bb9d6b328f1d67c84e1c9b583d3e
aleksandrsg <aleksandrs.galiullins@gmail.com>
2021-09-12 23:19:28 +0100

    Cart design

--- src/App.js
+++ src/App.js
@@ -6,21 +6,25 @@ function App() {
     <div className="App">
 
       <div className="wrapper clear">
-
         {/*Drawer or (Right side)*/}
         <div className="overlay">
           <div className="drawer">
-              <h2 className="mb-30">Cart</h2>
-              <div className="cartItem d-flex align-center justify-between mb-20">
-               <img className="mr-20" width={70} heigth={70} src="img/sneakers/1.jpg" alt="Sneakers" />
-                <div className="mr-20">
-                  <p className="mb-5">Here will be the descriptionof the sneakers </p>
-                  <b>Price: EUR20.00</b>
-               </div>
-              <button className="removeBtn">
-                <img  src="img/btn-remove.svg" alt="Remove"/>
-              </button>
-              </div>
+               <h2 className="d-flex justify-between mb-30">
+                 Cart <img  className="cu-p" src="img/btn-remove.svg" alt="Remove"/>
+               </h2>
+              
+              <div className="items">
+
+                <div className="cartItem d-flex align-center justify-between mb-20">
+                <img className="mr-20" width={70} heigth={70} src="img/sneakers/1.jpg" alt="Sneakers" />
+                  <div className="mr-20">
+                    <p className="mb-5">Here will be the descriptionof the sneakers </p>
+                    <b>Price: EUR20.00</b>
+                  </div>
+                <button className="removeBtn">
+                  <img  src="img/btn-remove.svg" alt="Remove"/>
+                </button>
+                </div>
 
               <div className="cartItem d-flex align-center justify-between mb-20">
                <img className="mr-20" width={70} heigth={70} src="img/sneakers/2.jpg" alt="Sneakers" />
@@ -31,6 +35,23 @@ function App() {
               <button className="removeBtn">
                 <img  src="img/btn-remove.svg" alt="Remove"/>
               </button>
+              </div>
+           
+              <div className="cartTotalBlock">
+              <ul>
+                  <li>
+                    <span>Total:</span>
+                    <div></div>
+                    <b>EUR 40.00</b>
+                  </li>
+                  <li>
+                    <span>VAT 5%:</span>
+                    <div></div>
+                    <b>EUR 2.00</b>
+                  </li>
+               </ul>
+               <button className='greenButton'>Complete Order <img src="img/arrow.svg" alt="arrow" /></button>
+              </div>
               </div>
           </div>
         </div>
